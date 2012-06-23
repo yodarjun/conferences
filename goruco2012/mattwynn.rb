e11e0c5437b8c3f2b41d0d7cc9a4a4726f9676cb
Arjun Anand <arjun.anand84@gmail.com>
2012-06-23 11:35:15 -0400

    Matt Wynn

---
+++ goruco2012/mattwynn.rb
@@ -0,0 +1,15 @@
+# rspec -d -f documentation ctap_poll.rb
+require 'rspec'
+
+describe "Hexagonal Rails" do
+  it "relish app; eg: documentation for rspec"
+  it "modularizing rails apps when the app's feature set because a deterrent for development"
+  it "adapter architecture from the gang of four book"
+  it "use Metaphors to model the domain of your application"
+  it "'Responsibility-Driven Design'"
+  it "RubyConf2011 - Designing protocols"
+  it "objects on rails mailing list"
+  context "Passive Controller" do
+    it "controllers should talk to the domain object and not do data manip"
+  end
+end
