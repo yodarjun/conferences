08adfa0635bfcc36951b856aca210e93bad67c49
Arjun Anand <arjun.anand84@gmail.com>
2012-06-23 10:43:13 -0400

    Dr. Nic

---
+++ goruco2012/drnic.rb
@@ -0,0 +1,19 @@
+# rspec -d -f documentation ctap_poll.rb
+require 'rspec'
+
+describe "Deployment of the future" do
+  it "drnic@engineyard.com"
+
+  it "package your code for public domain as well as enterprise very easily"
+  it "there shouldn't be 10 devs dedicated to packaging the public version to enterprise version(Github enterprise)"
+  it '"Our tools shape our thinking"'
+  it "rolling deployments should check a new version on 1 production server and then gets deployed to other prod servers if the 1st server runs satisfactorily"
+  it "tools that you use shouldn't hinder your thoughts and box it"
+  context "solutions to the packaging problem" do
+    context "do it yourself solutions" do
+      it "http://www.slideshare.net/AnthonyShortland/dto-chefconf2012"
+      it "BOSH: github.com/cloudfoundry/bosh"
+      it "- github.com/drnic/bosh-getting-started"
+    end
+  end
+end
