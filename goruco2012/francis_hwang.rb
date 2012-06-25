4e58411864bba51b27ec8f04d778027bf5fd064f
Arjun Anand <arjun.anand84@gmail.com>
2012-06-25 09:40:06 -0400

    francis hwang

---
+++ goruco2012/francis_hwang.rb
@@ -0,0 +1,19 @@
+# rspec -d -f documentation mattwynn.rb
+require 'rspec'
+
+describe "Front End Future" do
+  context "thick web client" do
+    context "cons" do
+      it "is harder to test"
+      it "difficult to crawl(SEO)"
+      it "is more prone to get out of date"
+    end
+
+    context "pros" do
+      it "REST is awesome"
+      it "DRY's up code"
+      it "* jQuery promises?"
+      it "Speed"
+    end
+  end
+end
