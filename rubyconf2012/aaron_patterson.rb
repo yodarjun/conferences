f845946d8a8c1365fd79eb35a745f707c6454d15
Arjun Anand <arjun.anand84@gmail.com>
2012-11-02 15:27:50 -0600

    Aaron Patterson and Masatoshi Kata

---
+++ rubyconf2012/aaron_patterson.rb
@@ -0,0 +1,36 @@
+require 'rspec'
+
+# https://github.com/tenderlove/tusk
+describe "Real Time Salami" do
+  context "meat stuff" do
+    it "really?????"
+  end
+
+  describe "work stuff" do
+    context "streaming templates" do
+      it "currently clients wait while rendeing is happening"
+      context "ActionController::Live" do
+        it "renders whatever is available in real time"
+        it "useful for Streaming ERB"
+        it "can be used for server sent events (real time auto-reloading of template pages)"
+      end
+    end
+  end
+
+  describe "hacker stuff" do
+    it "RubyVM::InstructionSequence.new(inst).disasm"
+    it "insns.def in ruby code base"
+    it "iseqs.def"
+
+    describe "microptimizations" do
+      it "benchmarking"
+      it "looking at code"
+      it "string interpolation in Ruby < 2.0 results in multiple string objects"
+      it "OPTIMIZE for readability first, then find bottlenecks, and then optimize"
+    end
+  end
+
+
+  # ruby -Ilib
+  # bug #7158
+end
---
+++ rubyconf2012/masatoshi_kata_makoto_inoue.rb
@@ -0,0 +1,18 @@
+require 'rspec'
+
+# druby book. 40% off "RubyConf2012"
+describe "Rails is a follower" do
+  it "dRuby and Rinda"
+
+  describe "dRuby" do
+    it "is a distributed object system that can invoke methods on different processes"
+    # try using in multiple consoles to interact with each other
+
+    it "used for distributed rspec at Cookpad"
+    # look at Starling by @blaine
+
+    it "shttpserv, precursor to webrick"
+
+    it "DRbUnknown error when DRb doenst understand the instruction set form another process"
+  end
+end
