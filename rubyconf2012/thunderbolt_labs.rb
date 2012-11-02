a104955776ff99f9054bc7d102f714e7f4364896
Arjun Anand <arjun.anand84@gmail.com>
2012-11-02 17:23:54 -0600

    Thunderbolt Labs

---
+++ rubyconf2012/thunderbolt_labs.rb
@@ -0,0 +1,43 @@
+require 'rspec'
+
+describe "Implementing RubyMotion" do
+  describe "Getting Started" do
+    it "motion create sample"
+    it "has a different way to define methods"
+  end
+
+  describe "Storyboards" do
+    it "lets you wireframe/prototype very quickly"
+  end
+
+  it "is a very beautiful illusion over a horrible land"
+
+  it "cocoapods is like gems for ios development"
+
+  it "developers need to be conscious about memory when developing for mobile"
+
+  it "lets you use lambdas to define callbacks"
+
+  it "Apple is a deathstar"
+
+  it "no matter what, you are still going to have to understand Cocoa and use some Objective C code"
+
+  describe "tips" do
+    it "IB gem: https://github.com/yury/ib"
+    it "Using Bundler: http://thunderboltlabs.com/posts/using-bundler-with-rubymotion"
+    it "CocoaPods: https://github.com/cocoapods"
+
+    context "AFNetworking: https://github.com/AFNetworking/AFNetworking" do
+      it "Streaming"
+      it "Progress Monitoring"
+      it "Authetication"
+      it "Caching"
+      it "Success Failures through HTTP codes"
+    end
+
+    it "SDWebImage: https://github.com/rs/SDWebImage"
+    it "try using GCD instead of threads"
+    it "RestKit: https://github.com/RestKit/RestKit"
+    it "https://github.com/josevalim/active_model_serializers: for JSON api apps"
+  end
+end
