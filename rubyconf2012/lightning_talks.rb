4d11372a3945f6f750ba296a1ffdbbcdc1d20d94
Arjun Anand <arjun.anand84@gmail.com>
2012-11-03 10:10:47 -0600

    lightning talks

---
+++ rubyconf2012/lightning_talks.rb
@@ -0,0 +1,17 @@
+require 'rspec'
+
+describe "Jeff Casimir" do
+  it "gschool; sister concern of jumpstart labs"
+end
+
+describe "sferik" do
+  it "gem install t. awesome cli twitter client"
+end
+
+describe "Joseph Wilk" do
+  it "neural network code for understanding haikus: https://github.com/josephwilk"
+end
+
+describe "game development" do
+  it "gem install metro"
+end
