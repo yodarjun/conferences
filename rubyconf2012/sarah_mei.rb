b9aeaa479e23bb087003e7b787a184c978c9b131
Arjun Anand <arjun.anand84@gmail.com>
2012-11-03 10:52:10 -0600

    Sarah Mei

---
+++ rubyconf2012/sarah_mei.rb
@@ -0,0 +1,41 @@
+require 'rspec'
+
+# @sarahmei
+# sarahmei@pivotallabs.com
+describe "The Inefficiency of good deisgn" do
+  it "depending on code that doesnt change often is better than depending on code that does change often"
+
+  describe "Factors affecting good code" do
+    it "know OO design pricinples"
+    it "know good development practices (agile, lean, etc)"
+    it "*other people"
+    it "informal and formal communication between teams determine the best kind of code"
+    it "The aspects of code that are most important are hidden because of their simplicity"
+    it "communication structures can impact code design"
+  end
+
+  describe "Good code predictors" do
+    it "techinical qualifications of the the dev"
+    it "experience with the code base"
+    it "most importantly good communication"
+  end
+
+  describe "Decoding smelly code" do
+    it "hidden silos"
+    it "developers ready for more nuance"
+    it "fixing the code and communication will help resolve the same kind of smelly code happening again"
+  end
+
+  describe "Fixing problems" do
+    it "pair programming"
+    it "create opportunities for informal communication"
+    it "create a space to relax in the office"
+    it "use high-badwidth communication (face-to-face, video chat)"
+    it "teach learn and practice"
+  end
+
+  describe "Good reads" do
+    it "Practical Object Oriented Design in Ruby"
+    it "Vacuum in delhi??????? newspaper clipping"
+  end
+end
